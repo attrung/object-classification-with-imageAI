e7f5d702c46356f55521166766753d55fe38cca6
Alyssa Vu <vua22@mail.wlu.edu>
2020-05-21 15:22:59 +0700

    tinyyolo

--- objectDetection.py
+++ objectDetection.py
@@ -5,8 +5,8 @@ execution_path = os.getcwd() + '/static/image/'
 
 def objectDetection(inputPath, outputPath, prob):
     detector = ObjectDetection()
-    detector.setModelTypeAsRetinaNet()
-    detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+    detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelPath( os.path.join(execution_path , "yolo-tiny.h5"))
     detector.loadModel()
     detections = detector.detectObjectsFromImage(input_image = os.path.join(execution_path, inputPath), output_image_path = os.path.join(execution_path, outputPath), minimum_percentage_probability = prob)
 
@@ -17,13 +17,4 @@ def objectDetection(inputPath, outputPath, prob):
         probList.append(eachObject["percentage_probability"])
     return nameList, probList
 
-def main():
-    inputPath = "static/upload/image.jpg"
-    outputPath = "static/upload/imagenew.jpg"
-    probability = 80
-
-    objectDetection(inputPath, outputPath, probability)
-
-if __name__ == "__main__":
-    main()
 
